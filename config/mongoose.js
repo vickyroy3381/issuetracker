2aecdaa9fa269134800fc9df76d230e6a85031ba
vickyroy3381 <128716321+vickyroy3381@users.noreply.github.com>
2023-11-18 16:23:38 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,8 +7,6 @@ let DB_URL = process.env.DB_URL;
 
 //connect to the database
 mongoose.connect(DB_URL);
-//mongoose.connect("mongodb://localhost:27017/Issue-Tracker");
-// acquire connection (to check if its successful)
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
